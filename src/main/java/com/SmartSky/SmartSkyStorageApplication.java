ea78872e983bc17af4ad1969a597171896b75b72
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-27 10:18:54 +0530

    Initial commit: Project created from start.spring.io with config Java 17,Mave, JAR, Spring 3.3.5, Dep : Spring Web, Security, Dev Tools, Lombok, Thymeleaf, MySQL, Data JPA, Validation.

---
+++ src/main/java/com/SmartSky/SmartSkyStorageApplication.java
@@ -0,0 +1,13 @@
+package com.SmartSky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSkyStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSkyStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartSky/SmartSkyStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartSky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSkyStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
